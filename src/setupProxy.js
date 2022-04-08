337220d653291d2bd15c2c771411b35042c663df
Sonny Nguyen <sonnynguyen163@gmail.com>
2022-04-07 23:14:05 -0700

    major fixes to http proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = app => {
   app.use('/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: 'http://localhost:3001/',
       changeOrigin: true
     })
   );
