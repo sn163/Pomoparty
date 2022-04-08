42f59fb5a62b91d10c49bd2f3d1bf4b3cbc27383
Sonny Nguyen <sonnynguyen163@gmail.com>
2022-04-07 23:56:57 -0700

    reconfigure ports

--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
 require('dotenv').config();
-const path = require('path')
-const express = require('express')
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const express = require('express');
+
 const app = express();
 
 // Have Node serve the files for the React app
-app.use(express.static(path.resolve(__dirname, '../build')))
 
 // Handle GET requests to /api route
 app.get('/api', (req, res) => {
-    res.json({message: 'Hello from server!'});
-})
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
-})
+  res.status(201).json({ message: 'Hello from server!' });
+});
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
 
-app.listen(PORT, ()=> {
-    console.log(`Server listening on ${PORT}`)
-})
+  app.use(express.static('build'));
+  // app.use(express.static(path.resolve(__dirname, '/build')))
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+
+  app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+  // app.get('*', (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, '/build', 'index.html'))
+  // })
+
+  // app.get("*", (req, res) =>
+
+  //   res.sendFile(path.resolve("build", "index.html"))
+
+  // );
+}
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use('/api',
-    createProxyMiddleware({
-      target: 'http://localhost:3001/',
-      changeOrigin: true
-    })
-  );
+  app.use(createProxyMiddleware('/api/*',{ target: 'http://localhost:3001/'}));
 };
