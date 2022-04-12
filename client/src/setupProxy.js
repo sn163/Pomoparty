300ee0db93cc27b75738623cc33a25283fc4804d
Sonny Nguyen <sonnynguyen163@gmail.com>
2022-04-12 00:01:24 -0700

    applied fixes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use('/api/*', createProxyMiddleware({ target: 'http://localhost:3001/'}));
+  app.use('/api/**', createProxyMiddleware({ target: 'http://localhost:3001/'}));
 };
