c5d63126e6c27e4374c45eadfe4c6f97e72db1d3
Sonny Nguyen <sonnynguyen163@gmail.com>
2024-03-30 01:05:09 -0700

    added timer, update nav, add more themes

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <div className="navbar nav-root">
+    <div className="navbar nav-root px-16">
       <div className="flex-1">
         <Image
           src="/pomoparty-logo.png"
@@ -12,21 +12,15 @@ export default function NavBar() {
           alt="pomoparty-logo"
         />
       </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
+      <div className="flex justify-center space-x-6">
+        <a>
+          <button className="text-white">Features</button>
+        </a>
+        <a>
+          <button className="text-white">Sign In</button>
+        </a>
+        <button className="btn rounded-full btn-primary-content text-primary border-2 shadow-md">
+          Sign Up
         </button>
       </div>
     </div>
