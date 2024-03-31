96979107b7b2ed3ac5cd129c36136700a6747001
aleckslu <alecks.lu@gmail.com>
2024-03-31 05:36:36 -0700

    links to contact, features, signin in Navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,28 +1,32 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
-  return (
-    <div className="navbar nav-root drop-shadow-lg opacity-95 px-16 py-3">
-      <a className="flex-1" href="/">
-        <Image
-          src="/pomoparty-logo-white.png"
-          height={50}
-          width={200}
-          alt="pomoparty-logo"
-        />
-      </a>
-      <div className="flex justify-center space-x-6">
-        <a>
-          <button className="text-white">Features</button>
-        </a>
-        <a>
-          <button className="text-white">Sign In</button>
-        </a>
-        <button className="btn rounded-full btn-primary-content text-primary border-2 shadow-md">
+	return (
+		<div className="navbar nav-root drop-shadow-lg opacity-95 px-16 py-3">
+			<a className="flex-1" href="/">
+				<Image
+					src="/pomoparty-logo-white.png"
+					height={50}
+					width={200}
+					alt="pomoparty-logo"
+				/>
+			</a>
+			<div className="flex justify-center space-x-6">
+				<Link href="/contact" passHref>
+					<button className="text-white">Contact</button>
+        </Link>
+				<Link href="/features" passHref>
+					<button className="text-white">Features</button>
+				</Link>
+				<Link href="/signin" passHref>
+					<button className="text-white">Sign In</button>
+				</Link>
+				<button className="btn rounded-full btn-primary-content text-primary border-2 shadow-md">
           Sign Up
         </button>
       </div>
     </div>
-  );
+	);
 }
