cc9a8b1a5a5f537ce92491773b8f87fb792ac1d8
aleckslu <alecks.lu@gmail.com>
2024-03-31 01:55:48 -0700

    removed navbar from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 import Dashboard from "./_components/Dashboard";
-import NavBar from './_components/Navbar';
 
 export default function Home() {
-	return (
-		<>
-			<NavBar/>
-			<Dashboard startHr={0} startMin={0} startSec={5} />
-		</>
-	);
+	return <Dashboard startHr={0} startMin={1} startSec={20} />;
 }
