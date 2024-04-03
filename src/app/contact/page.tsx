5086290c3a84ec8def8cb144ab77ab3d0852ee2f
aleckslu <alecks.lu@gmail.com>
2024-04-03 00:24:23 -0700

    before pull

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,131 +2,122 @@
 
 // import { useState } from "react";
 
-type selectCategory = "none" | "account" | "bug" | "feedback" | "other";
+// type selectCategory = "none" | "account" | "bug" | "feedback" | "other";
 
 type ContactData = {
   name: string;
   email: string;
   category: selectCategory;
   details: string;
-  somehingelse: string;
 };
-const ContactUsSelectCategories = {
 
-}
-
+const ContactUsSelectCategories = {};
 
 export default function Contact() {
-//   // const [name, setName] = useState("");
-//   // const [email, setEmail] = useState("");
-//   // const [category, setCategory] = useState("none");
-//   // const [details, setDetails] = useState("");
-
-//   const inputComponent = (type:string,name:string,options={
-//     label: false,
-//     labelInnerHtml: ''
-//   },d) => {
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [category, setCategory] = useState("none");
+  // const [details, setDetails] = useState("");
 
 
+  const handleSubmit = (e): void => {
 
-//     return (
-//       <>
-//       {options.label ? <label></label>}
-//         <input type={type} name={name} className=''>
-//       </>
-//     )
-//   }
+    //  const formToBeSubmitted = {
+    //     name: e.target.name.name,
+    //     value: e.target.name.value
+    //  }
 
-  const handleSubmit = (e):void => {
-    console.log("handleSubmit()");
+        // console.log(e.target.email.name);
+    // console.log(e.target.email.value);
+    console.log(e.target);
+    const {name} = e.target;
+    console.log(name)
     e.preventDefault();
-    console.log(e.target)
-    // const val = e.target ;
-    // const formToBeSubmitted: ContactData = {
-    //  name : e.target[0] 
-    // }
+    const formToBeSubmitted = {
+      {name}
+    };
 
-    // for (let el of e.target) {
-    //   console.log("looping through e", el.value);
-    //   formToBeSubmitted[el.name] = el.value;
-    // }
+  // const handleInputs =
+  //   (inputField: string) => (event: ChangeEvent<HTMLInputElement>) => {
+  //     const { value } = event.target;
+  //     setTimeUnitsInput((prevState) => ({
+  //       ...prevState,
+  //       [inputField]: +value,
+  //     }));
+  //   };
 
-    // console.log("submit: ", e.target);
+
+
+    // Object.assign(
+    //   {},
+    //   ...e.target.map(({ name, value }) => ({ [name]: value })),
+    // );
+
+    console.log("formToBeSubmitted: ", formToBeSubmitted);
+
+    console.log(formToBeSubmitted);
   };
 
-  // const handleChange = (e) => {
-  //   const val = e.target.value;
-
-  //   // console.log(e.target.type)
-  //   console.log(e.target.name + ": " + val);
-  //   // console.log("val: ", val)
-  //   console.log({ name, email, category, details });
-  // }; space-y-10'
-  
   return (
-    <div 
-    className='flex flex-col items-center my-20'
-    // hidden={true} 
-    onSubmit={handleSubmit } >
-      <h1>Contact Us</h1>
+    <main className="flex flex-col items-center space-y-4 p-20">
+      <div className="my-20 flex flex-col items-center" onSubmit={handleSubmit}>
+        <h1>Contact Us</h1>
 
-      <form className='flex flex-col items-center space-y-5'>
-        {/* name */}
-        <label className="input input-bordered flex items-center gap-3">
-          Name
-          <input
-            name="name"
+        <form className="flex flex-col items-center space-y-3">
+          {/* name */}
+          <label className="input input-bordered flex items-center gap-3">
+            Name
+            <input
+              name="name"
+              // onChange={handleChange}
+              type="text"
+              className="grow"
+              placeholder="Pomo Doro"
+            />
+          </label>
+          {/* email */}
+          <label className="input input-bordered flex items-center gap-3">
+            Email
+            <input
+              name="email"
+              // onChange={handleChange}
+              type="email"
+              className="grow"
+              placeholder="my@email.com"
+            />
+          </label>
+          {/* select category */}
+          <select
+            name="category"
             // onChange={handleChange}
-            type="text"
-            className="grow"
-            placeholder="Pomo Doro"
-          />
-        </label>
-        {/* email */}
-        <label className="input input-bordered flex items-center gap-3">
-          Email
-          <input
-            name="email"
+            className="form-control select select-bordered flex items-center"
+          >
+            <option disabled selected>
+              Pick a category
+            </option>
+            {categories}
+          </select>
+          {/* explain yoself */}
+          <textarea
+            name="details"
             // onChange={handleChange}
-            type="email"
-            className="grow"
-            placeholder="my@email.com"
-          />
-        </label>
-        {/* select category */}
-        <select
-          name="category"
-          // onChange={handleChange}
-          className="form-control select select-bordered flex items-center"
-        >
-          <option value="none" disabled selected>
-            Pick a category
-          </option>
-          <option value="account">Something Is Wrong With My Account</option>
-          <option value="bug">Report a Bug</option>
-          <option value="feedback">Feedback</option>
-          <option value="other">Other</option>
-        </select>
-        {/* explain yoself */}
-        <textarea
-          name="details"
-          // onChange={handleChange}
-          placeholder="Please provide more details"
-          className="textarea textarea-bordered flex items-center gap-2 bg-base-100"
-        ></textarea>
-        {/* button */}
-        <button
-          type="submit"
-          value="Submit"
-          // onClick={(e) => {
-          //   console.log("in onClick: ", e.target);
-          //   // e.preventDefault();
-          // }}
-          className="btn-main rounded-full"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+            placeholder="Please provide more details"
+            className="textarea textarea-bordered flex items-center gap-2 bg-base-100"
+          ></textarea>
+          {/* button */}
+          <button
+            type="submit"
+            value="Submit"
+            // onClick={(e) => {
+            //   console.log("in onClick: ", e.target);
+            //   // e.preventDefault();
+            // }}
+            className="btn-main rounded-full"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
