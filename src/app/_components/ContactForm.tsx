4d13d6d4ab09f9d1b1b1f08f6b796f98b015e60a
aleckslu <alecks.lu@gmail.com>
2024-04-04 04:52:51 -0700

    set up contact page for ssr

--- src/app/_components/ContactForm.tsx
+++ src/app/_components/ContactForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { submitWeb3Form } from "@/actions/handleWeb3"
+import { submitWeb3Form } from "@/actions/handleWeb3";
 // import { useState } from "react";
 
 type selectCategory = "none" | "account" | "bug" | "feedback" | "other";
@@ -31,75 +31,61 @@ export default function Contact() {
 
   return (
     <>
-      <div className="hero bg-base-200">
-        <div className="hero-content xl:flex-col">
-          
-          <div className="min-w-xl card w-full max-w-2xl shrink-0 bg-base-100 shadow-2xl xl:card-side">
-            <form className="card-body" onSubmit={submitWeb3Form}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">
-                    I&apos;d like to chat about...
-                  </span>
-                </label>
-                <select
-                  name="category"
-                  className="select select-bordered"
-                  required
-                >
-                  <option disabled selected>
-                    - Please Select -
-                  </option>
-                  {categories}
-                </select>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="John Doe"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="my@email.com"
-                  className="input input-bordered"
-                  // onChange={(event) => setEmail(event.target.value)}
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Your Message</span>
-                </label>
-
-                <textarea
-                  name="message"
-                  placeholder="Please enter your message here."
-                  className="textarea textarea-bordered resize-none"
-                  required
-                />
-              </div>
-
-              <div className="form-control mt-6">
-                <button type="submit" className="btn btn-primary">
-                  Submit Form
-                </button>
-              </div>
-            </form>
-          </div>
+      <form className="card-body" onSubmit={submitWeb3Form}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">I&apos;d like to chat about...</span>
+          </label>
+          <select name="category" className="select select-bordered" required>
+            <option disabled selected>
+              - Please Select -
+            </option>
+            {categories}
+          </select>
         </div>
-      </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="my@email.com"
+            className="input input-bordered" // onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Message</span>
+          </label>
+
+          <textarea
+            name="message"
+            placeholder="Please enter your message here."
+            className="textarea textarea-bordered resize-none"
+            required
+          />
+        </div>
+
+        <div className="form-control mt-6">
+          <button type="submit" className="btn btn-primary">
+            Submit Form
+          </button>
+        </div>
+      </form>
     </>
   );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,13 @@ export default function ContactPage() {
       <div className="xl:text-top text-center">
         <h1 className="text-5xl font-bold">Contact Us</h1>
       </div>
-      <ContactForm />
+      <div className="hero bg-base-200">
+        <div className="hero-content xl:flex-col">
+          <div className="min-w-xl card w-full max-w-2xl shrink-0 bg-base-100 shadow-2xl xl:card-side">
+            <ContactForm />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
