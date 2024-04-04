4dc2561989e7a0389b019d560a59142ecdf6c583
cyliang93 <cathyluong93@yahoo.com>
2024-04-03 19:47:33 -0700

    added src/_assets folder

--- src/app/_components/TaskList.tsx
+++ src/app/_components/TaskList.tsx
@@ -1,75 +1,76 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function TaskList() {
-    const [taskList, setTaskList] = useState<string[]>([]);
-    const [task, setNewTask] = useState<string>('');
+  const [taskList, setTaskList] = useState<string[]>([]);
+  const [task, setNewTask] = useState<string>("");
 
-    const addedTasks = (text:string) => {
-        const newTaskList = [...taskList, text];
-        setTaskList(newTaskList);
-        setNewTask('');
-    }
+  const addedTasks = (text: string) => {
+    const newTaskList = [...taskList, text];
+    setTaskList(newTaskList);
+    setNewTask("");
+  };
 
-    const deleteTasks = (id:string) => {
-        const removeTask = taskList.filter((task) => {
-            return task !== id;
-        })
-        setTaskList(removeTask);
-    }
+  const deleteTasks = (id: string) => {
+    const removeTask = taskList.filter((task) => {
+      return task !== id;
+    });
+    setTaskList(removeTask);
+  };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNewTask(event.target.value);
-    };
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value);
+  };
 
-    return(
-    <>
-    <div 
-    style={{display:'flex', justifyContent:'center', alignItems:'center', margin:'1.5rem', fontSize:'25px'}}
-    className="btn-main rounded-full"
-    >
+  return (
+    <div className="flex flex-col items-center justify-center pt-10">
+      <div className="btn btn-accent border-2 text-lg text-primary shadow-md">
         <h1>To-Do List: </h1>
-    </div>
-    <div>
-        <input 
-        type="text"
-        value={task} 
-        onChange={handleInputChange} 
-        placeholder='Enter task'
-        style={{
-            border:'3px solid #FF8989', 
-            borderRadius:5, 
-            height:'45px', 
-            width:'300px', 
-            margin:'1rem', 
-            whiteSpace:'normal',
-            padding:'1rem',
-        }}
+      </div>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={handleInputChange}
+          placeholder="Enter Task"
+          // className='border-current round-md #FF8989 h-10 w-60 m-6 whitespace-normal pt-2'
+          style={{
+            border: "3px solid #FF8989",
+            borderRadius: 5,
+            height: "45px",
+            width: "300px",
+            margin: "1rem",
+            whiteSpace: "normal",
+            padding: "1rem",
+          }}
         />
-        <button 
-        className="btn-main rounded-full "
-        type='button'
-        onClick={() => {addedTasks(task)}}
-        > 
-            Add Task 
+        <button
+          className="btn btn-accent mt-1 border-2 text-primary shadow-md"
+          type="button"
+          onClick={() => {
+            addedTasks(task);
+          }}
+        >
+          Add Task
         </button>
-    </div>
-    <div>
+      </div>
+      <div>
         <ul>
-            {taskList.map((task, index) => (
-                <li key={index} className='text-wrap'> 
-                {task} 
-                <button
-                className="btn-main rounded-full "
-                type='button'
-                style={{margin:'.5rem'}}
-                onClick={() => {deleteTasks(task)}}
-                > 
-                    Delete 
-                </button> 
-                </li>
-            ))}
+          {taskList.map((task, index) => (
+            <li key={index} className="text-wrap">
+              {task}
+              <button
+                className="btn btn-accent m-3 border-2 text-primary shadow-md"
+                type="button"
+                onClick={() => {
+                  deleteTasks(task);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
         </ul>
+      </div>
     </div>
-    </>
-    );
-};
+  );
+}
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,3 @@
 export default function SignIn() {
-    return(
-    <div>SignIn</div>
-    
-    );
-};
+  return <div>SignIn</div>;
+}
