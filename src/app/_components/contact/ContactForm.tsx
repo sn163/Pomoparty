76a883bae49ef354d439c760fd0d98ae5df33d30
aleckslu <alecks.lu@gmail.com>
2024-04-12 04:20:04 -0700

    instant form checking

--- src/app/_components/contact/ContactForm.tsx
+++ src/app/_components/contact/ContactForm.tsx
@@ -2,6 +2,7 @@
 
 import { submitWeb3Form } from "@/app/_actions/handleWeb3";
 import React, {
+  ChangeEvent,
   ReactHTMLElement,
   useCallback,
   useEffect,
@@ -60,8 +61,49 @@ export default function ContactForm() {
     [],
   );
 
-  const handleCategoryChange = (event) => {
+  const validateForm = (inputName: string, value: string) => {
+    type ValidityChecker = (input: string) => boolean;
+    interface ValidyCheckers {
+      [key: string]: ValidityChecker;
+    }
+
+    const formValidationRules: ValidyCheckers = {
+      name: (input) => input.length >= 2,
+      email: (input) => {
+        const regex =
+          /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+        return regex.test(input);
+      },
+      category: (input) => input !== "none-selected",
+      message: (input) => input.length > 12 && input.length < 200,
+    };
+
+    const isValid = formValidationRules[inputName](value);
+    console.log(isValid);
+    setFormValidity({
+      ...formValidity,
+      [inputName]: isValid,
+    });
+  };
+
+  const handleCategoryChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setCategory(event.target.value);
+    validateForm(event.target.name, event.target.value);
+  };
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setName(event.target.value);
+    validateForm(event.target.name, event.target.value);
+  };
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setName(event.target.value);
+    validateForm(event.target.name, event.target.value);
+  };
+
+  const handleMessageChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setName(event.target.value);
+    validateForm(event.target.name, event.target.value);
   };
 
   const formRef = useRef<HTMLFormElement>(null);
@@ -111,6 +153,7 @@ export default function ContactForm() {
             name="name"
             placeholder="John Doe"
             className="input input-bordered"
+            onChange={handleNameChange}
             required
           />
         </div>
@@ -123,6 +166,7 @@ export default function ContactForm() {
             name="email"
             placeholder="my@email.com"
             className="input input-bordered"
+            onChange={handleEmailChange}
             required
           />
         </div>
@@ -135,6 +179,7 @@ export default function ContactForm() {
             name="message"
             placeholder="Please enter your message here."
             className="textarea textarea-bordered resize-none"
+            onChange={handleMessageChange}
             required
           />
         </div>
