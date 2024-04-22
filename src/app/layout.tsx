7b5d71977b4a6ed0f3c9686404ae88f66bce801a
cyliang93 <cathyluong93@yahoo.com>
2024-04-21 21:59:08 -0700

    import dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Rowdies } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import NavBar from "./_components/Navbar";
+import TaskList from "./_components/TaskList";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const rowdies = Rowdies({
@@ -27,6 +28,7 @@ export default function RootLayout({
         <NavBar />
         {children}
         <SpeedInsights />
+        <TaskList />
       </body>
     </html>
   );
