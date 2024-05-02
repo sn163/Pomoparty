61d94a0a86d7b9f17da1b1a500e01424c09796fc
cyliang93 <cathyluong93@yahoo.com>
2024-05-01 22:22:35 -0700

    Login Modal

--- src/app/_components/Login.tsx
+++ src/app/_components/Login.tsx
@@ -1,22 +1,51 @@
 import { useState } from "react";
+import SignIn from "./Navbar";
 
 export default function LoginModal() {
   const [modal, setModal] = useState(false);
+  const [signIn, setSignIn] = useState(false);
 
-  const handleChange = () => {
-    setModal(!modal);
+  const handleOnChange = () => {
+    setSignIn(!signIn);
+  };
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
   };
 
   return (
     <div>
-      <div>
+      {/* <div>
         <h1>Username</h1>
         <input type="text" placeholder="Enter Username" />
       </div>
       <div>
         <h1>Password</h1>
         <input type="text" placeholder="Password" />
-      </div>
+      </div> */}
+
+      <button type="button" onClick={handleOnChange}>
+        openModal
+      </button>
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* <!-- if there is a button in form, it will close the modal --> */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 }
